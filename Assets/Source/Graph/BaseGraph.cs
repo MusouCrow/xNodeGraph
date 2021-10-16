f312acdf1d36d04ace0fa94bc28bf70293b9800c
MusouCrow <939549924@qq.com>
2021-10-16 22:34:22 +0800

    Fix stack overflow about graph

--- Assets/Source/Graph/BaseGraph.cs
+++ Assets/Source/Graph/BaseGraph.cs
@@ -15,24 +15,32 @@ namespace Game.Graph {
         protected void OnEnable() {
             this.funcMap = new Dictionary<string, Func>();
             
+            var nodeCache = new Dictionary<Node, bool>();
+            
             foreach (var node in this.nodes) {
                 var n = node as FuncNode;
 
                 if (n) {
-                    bool async = this.IsAsyncNode(n);
+                    bool async = this.IsAsyncNode(n, nodeCache);
                     this.funcMap.Add(n.func, new Func() {node = n, async = async});
                 }
             }
         }
 
-        private bool IsAsyncNode(BaseNode node) {
+        private bool IsAsyncNode(BaseNode node, Dictionary<Node, bool> cache) {
+            if (cache.ContainsKey(node)) {
+                return cache[node];
+            }
+
             if (node.Async) {
+                cache.Add(node, true);
                 return true;
             }
             else if (node is CallNode) {
                 var callNode = node as CallNode;
 
                 if (callNode.watting) {
+                    cache.Add(node, true);
                     return true;
                 }
             }
@@ -40,6 +48,7 @@ namespace Game.Graph {
                 var callNode = node as CallGraphNode;
 
                 if (callNode.watting) {
+                    cache.Add(node, true);
                     return true;
                 }
             }
@@ -48,10 +57,12 @@ namespace Game.Graph {
                 var trueNode = branchNode.GetPortNode("True");
                 var falseNode = branchNode.GetPortNode("False");
 
-                if (trueNode && this.IsAsyncNode(trueNode)) {
+                if (trueNode && this.IsAsyncNode(trueNode, cache)) {
+                    cache.Add(node, true);
                     return true;
                 }
-                else if (falseNode && this.IsAsyncNode(falseNode)) {
+                else if (falseNode && this.IsAsyncNode(falseNode, cache)) {
+                    cache.Add(node, true);
                     return true;
                 }
             }
@@ -70,10 +81,12 @@ namespace Game.Graph {
             }
             */
 
-            if (node.NextNode && this.IsAsyncNode(node.NextNode)) {
+            if (node.NextNode && this.IsAsyncNode(node.NextNode, cache)) {
+                cache.Add(node, true);
                 return true;
             }
 
+            cache.Add(node, false);
             return false;
         }
     }
